873f380c239dd9efbeda1c4534a6136c6b19d34f
VirajAdiga <virajadigamaclub@gmail.com>
2024-04-23 10:00:42 +0530

    Use ip address to rate limit if user not authenticated

--- rate_limiter/middlewares/middlewares.py
+++ rate_limiter/middlewares/middlewares.py
@@ -1,9 +1,10 @@
 import redis
 from django.conf import settings
 
-from django.http import HttpResponseForbidden
+from django.http import HttpResponse
 
 MAX_REQUESTS_PER_MINUTE = 5
+RATELIMIT_CACHE_KEY_PREFIX = "ratelimit"
 
 
 class RateLimitMiddleware:
@@ -12,17 +13,25 @@ class RateLimitMiddleware:
         self.redis_client = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
     def __call__(self, request):
+        # If user is authenticated, use user id
         if request.user.is_authenticated:
             user_id = request.user.id
-            redis_key = f"ratelimit:{user_id}"
-
-            current_requests = self.redis_client.incr(redis_key)
-            self.redis_client.expire(redis_key, 60)
-
-            if current_requests > MAX_REQUESTS_PER_MINUTE:
-                return HttpResponseForbidden("Rate Limit Exceeded")
+            redis_key = f"{RATELIMIT_CACHE_KEY_PREFIX}:{user_id}"
         else:
-            print("No rate limiting is applicable")
+            # If user is not authenticated
+            # Get IP address from the request
+            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            if x_forwarded_for:
+                ip = x_forwarded_for.split(',')[0]  # If user is behind proxy
+            else:
+                ip = request.META.get('REMOTE_ADDR')  # If not behind a proxy, get direct IP
+            redis_key = f"{RATELIMIT_CACHE_KEY_PREFIX}:{ip}"
+
+        current_requests = self.redis_client.incr(redis_key)
+        self.redis_client.expire(redis_key, 60)
+
+        if current_requests > MAX_REQUESTS_PER_MINUTE:
+            return HttpResponse("Too many requests! Rate Limit Exceeded", status=429)
 
         response = self.get_response(request)
         return response
--- rate_limiter/rate_limiter/settings.py
+++ rate_limiter/rate_limiter/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'greet_user'
+LOGIN_REDIRECT_URL = 'greet-user'
 
 REDIS_HOST = 'localhost'
 REDIS_PORT = '6379'
